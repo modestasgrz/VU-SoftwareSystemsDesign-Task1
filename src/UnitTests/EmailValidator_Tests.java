d052ed43fb40c6f3728a767986a90cb572428210
modestasgrz <modestas.grazys@mif.stud.vu.lt>
2021-09-17 15:37:21 +0300

    Tests updated

--- src/UnitTests/EmailValidator_Tests.java
+++ src/UnitTests/EmailValidator_Tests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class EmailValidator_Tests {
 
